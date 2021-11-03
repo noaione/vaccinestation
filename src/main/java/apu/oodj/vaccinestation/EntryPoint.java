f881225820957c775c565f616fe0491f7b4e8ed6
noaione <noaione0809@gmail.com>
2021-11-03 10:20:41 +0800

    Bootstrap proejct

---
+++ src/main/java/apu/oodj/vaccinestation/EntryPoint.java
@@ -0,0 +1,13 @@
+package apu.oodj.vaccinestation;
+
+/**
+ * Hello world!
+ *
+ */
+public class EntryPoint 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Initializing System...");
+    }
+}
